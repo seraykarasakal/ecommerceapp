d0a3cbe967626baf4dda0d1e1235d454dbd7eacf
Seray Karasakal <seraykrskl@gmail.com>
2024-01-06 00:56:11 +0300

    updated design

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -66,48 +66,42 @@ const DashboardScreen = () => {
     };
     let listProductsView = (item) => {
         return (
-            <ScrollView key={item.product_id} style={styles.card}>
-                <TouchableOpacity onPress={() => navigation.navigate("EditProduct", { id: item.product_id })}>
-                    <ChevronDoubleRightIcon size="20" color="black" />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => deleteProduct(item.product_id)}>
-                    <TrashIcon size="20" color="black" />
-                </TouchableOpacity>
-                <Text style={styles.textheader}>Id</Text>
-                <Text style={styles.textbottom}>{item.product_id}</Text>
+            <View key={item.product_id} style={styles.card}>
+                <View style={styles.productContainer}>
+                    <Text style={styles.textheader}>Id</Text>
+                    <Text style={styles.textbottom}>{item.product_id}</Text>
 
-                <Text style={styles.textheader}>Product Name</Text>
-                <Text style={styles.textbottom}>{item.product_name}</Text>
+                    <Text style={styles.textheader}>Product Name</Text>
+                    <Text style={styles.textbottom}>{item.product_name}</Text>
 
-                <Text style={styles.textheader}>Product Description</Text>
-                <Text style={styles.textbottom}>{item.product_description}</Text>
+                    <Text style={styles.textheader}>Product Description</Text>
+                    <Text style={styles.textbottom}>{item.product_description}</Text>
 
-                <Text style={styles.textheader}>Product Price</Text>
-                <Text style={styles.textbottom}>{item.product_price}</Text>
-            </ScrollView>
+                    <Text style={styles.textheader}>Product Price</Text>
+                    <Text style={styles.textbottom}>{item.product_price}</Text>
+                </View>
+                <View style={styles.ıconContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate("EditProduct", { id: item.product_id })}>
+                        <ChevronDoubleRightIcon size="20" color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => deleteProduct(item.product_id)}>
+                        <TrashIcon size="20" color="black" />
+                    </TouchableOpacity>
+                </View>
+            </View>
         );
     };
     return (
         <View style={styles.container}>
             <Header label="Dashboard Screen" />
-            <ScrollView>
-                <View style={styles.content}>
-                    <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
-                        <Text style={styles.buttonText}>Logout</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <ArrowLeftIcon size="20" color="black" />
-                    </TouchableOpacity>
-
-                    <FlatList
-                        style={{ marginTop: 30, flex: 1 }}
-                        contentContainerStyle={{ paddingHorizontal: 20 }}
-                        data={flatListItems}
-                        keyExtractor={(item, index) => index.toString()}
-                        renderItem={({ item }) => listProductsView(item)}
-                    />
-                </View>
+            <ScrollView style={styles.content}>
+                <FlatList
+                    style={{ marginTop: 30 }}
+                    contentContainerStyle={styles.cardContainer}
+                    data={flatListItems}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item }) => listProductsView(item)}
+                />
             </ScrollView>
             <Navbar navigation={navigation} />
         </View>
@@ -117,20 +111,44 @@ const deviceWidth = Math.round(Dimensions.get("window").width);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#877dfa",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        paddingBottom: 70,
+        backgroundColor: "white",
+        paddingBottom: 10,
     },
     content: {
-        flex: 1,
-        backgroundColor: "#877dfa",
-        justifyContent: "flex-start",
-        padding: 10,
+        backgroundColor: "white",
         margin: 10,
     },
+    cardContainer: {
+        backgroundColor: "white",
+        justifyContent: "space-evenly",
+        marginBottom: 6,
+    },
+    card: {
+        flexDirection: "row",
+        backgroundColor: "#a29bfe",
+        borderRadius: 20,
+        shadowColor: "grey",
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 0.75,
+        marginTop: 20,
+        padding: 30,
+    },
+    ıconContainer: {
+        gap: 10,
+        flexDirection: "row",
+        width: "30%",
+        justifyContent: "flex-end",
+        alignItems: "flex-start",
+    },
+    productContainer: {
+        width: "70%",
+    },
+
     text: {
-        fontSize: 18,
+        fontSize: 12,
         marginBottom: 20,
     },
     logoutButton: {
@@ -146,31 +164,31 @@ const styles = StyleSheet.create({
     },
     textheader: {
         color: "#111",
-        fontSize: 12,
+        fontSize: 16,
         fontWeight: "700",
     },
     textbottom: {
         color: "#111",
         fontSize: 18,
     },
-    card: {
-        flex: 1,
-        width: deviceWidth - 70,
-        backgroundColor: "#a29bfe",
-        height: 300,
-        borderRadius: 20,
+    // card: {
+    //     flex: 1,
+    //     width: deviceWidth - 70,
+    //     backgroundColor: "#a29bfe",
+    //     height: 300,
+    //     borderRadius: 20,
 
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 5,
-            height: 5,
-        },
-        shadowOpacity: 0.75,
-        shadowRadius: 5,
-        elevation: 9,
-        marginTop: 20,
-        padding: 30,
-    },
+    //     shadowColor: "#000",
+    //     shadowOffset: {
+    //         width: 5,
+    //         height: 5,
+    //     },
+    //     shadowOpacity: 0.75,
+    //     shadowRadius: 5,
+    //     elevation: 9,
+    //     marginTop: 20,
+    //     padding: 30,
+    // },
 });
 
 export default DashboardScreen;
--- screens/EditProductScreen.js
+++ screens/EditProductScreen.js
@@ -1,19 +1,19 @@
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { useEffect, useState } from 'react';
-import { Alert, Button, KeyboardAvoidingView, SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, Button, KeyboardAvoidingView, SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
-import { DatabaseConnection } from '../config/database-connection';
+import { DatabaseConnection } from "../config/database-connection";
 const db = DatabaseConnection.getConnection();
-
+import Header from "./Header";
 
 const EditProduct = () => {
     const navigation = useNavigation();
     const route = useRoute();
     const id = route.params?.id;
 
-    let [productName, setProductName] = useState('');
-    let [productDescription, setProductDescription] = useState('');
-    let [productPrice, setProductPrice] = useState('');
+    let [productName, setProductName] = useState("");
+    let [productDescription, setProductDescription] = useState("");
+    let [productPrice, setProductPrice] = useState("");
 
     let updateAllStates = (name, description, price) => {
         setProductName(name);
@@ -21,112 +21,191 @@ const EditProduct = () => {
         setProductPrice(price);
     };
 
-
     useEffect(() => {
         console.log(id);
         db.transaction((tx) => {
-            tx.executeSql(
-                'SELECT * FROM table_products where product_id = ? ',
-                [id],
-                (tx, results) => {
-                    if (results.rows.length > 0) {
-                        updateAllStates(
-                            results.rows.item(0).product_name,
-                            results.rows.item(0).product_description,
-                            results.rows.item(0).product_price
-                        );
-                    } else {
-                        alert('Ürün bulunamadı');
-                        updateAllStates('', '', '');
-                    }
+            tx.executeSql("SELECT * FROM table_products where product_id = ? ", [id], (tx, results) => {
+                if (results.rows.length > 0) {
+                    updateAllStates(results.rows.item(0).product_name, results.rows.item(0).product_description, results.rows.item(0).product_price);
+                } else {
+                    alert("Ürün bulunamadı");
+                    updateAllStates("", "", "");
                 }
-            )
+            });
         });
     }, []);
-
-
-
 
     let editProduct = () => {
         console.log(id, productName, productDescription, productPrice);
 
         if (!productName) {
-            alert('Lütfen İsim Giriniz')
+            alert("Lütfen İsim Giriniz");
         }
         if (!productDescription) {
-            alert('Lütfen Açıklama Giriniz')
+            alert("Lütfen Açıklama Giriniz");
         }
         if (!productPrice) {
-            alert('Lütfen Fiyat Giriniz')
+            alert("Lütfen Fiyat Giriniz");
         }
 
         db.transaction((tx) => {
             tx.executeSql(
-                'UPDATE table_products set product_name=?, product_description=?, product_price=? where product_id=?',
+                "UPDATE table_products set product_name=?, product_description=?, product_price=? where product_id=?",
                 [productName, productDescription, productPrice, id],
                 (tx, results) => {
-                    console.log('Results', results.rowsAffected);
+                    console.log("Results", results.rowsAffected);
                     if (results.rowsAffected > 0) {
                         Alert.alert(
-                            'Başarılı',
-                            'Ürün güncelleme başarılı oldu!',
+                            "Başarılı",
+                            "Ürün güncelleme başarılı oldu!",
                             [
                                 {
-                                    text: 'Ok',
-                                    onPress: () => navigation.navigate('ListProducts'),
+                                    text: "Ok",
+                                    onPress: () => navigation.navigate("ListProducts"),
                                 },
                             ],
                             { cancelable: false }
                         );
-                    } else alert('Kullanıcı güncelleme başarısız!');
+                    } else alert("Kullanıcı güncelleme başarısız!");
                 }
             );
         });
-
     };
 
+    // return (
+    //     <SafeAreaView style={{ flex: 1 }}>
+    //         <View style={{ padding: 20 }}>
+    //             <TouchableOpacity onPress={() => navigation.goBack()} className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
+    //                 <ArrowLeftIcon size="20" color="black" />
+    //             </TouchableOpacity>
+    //         </View>
+    //         <View style={{ flex: 1, backgroundColor: "white" }}>
+    //             <View style={{ flex: 1 }}>
+    //                 <ScrollView keyboardShouldPersistTaps="handled">
+    //                     <KeyboardAvoidingView behavior="padding" style={{ flex: 1, justifyContent: "space-between" }}>
+    //                         <Text> Ürün Güncelleme Ekranı </Text>
+    //                         <TextInput
+    //                             placeholder="Ürün Adı"
+    //                             value={productName}
+    //                             editable={true}
+    //                             style={{ padding: 10 }}
+    //                             onChangeText={(productName) => setProductName(productName)}
+    //                         />
+    //                         <TextInput
+    //                             placeholder="Ürün Açıklaması"
+    //                             value={productDescription}
+    //                             editable={true}
+    //                             maxLength={255}
+    //                             numberOfLines={5}
+    //                             multiline={true}
+    //                             style={{ padding: 10 }}
+    //                             onChangeText={(productDescription) => setProductDescription(productDescription)}
+    //                         />
+    //                         <TextInput
+    //                             placeholder="Ürün Fiyatı"
+    //                             value={productPrice.toString()}
+    //                             editable={true}
+    //                             maxLength={5}
+    //                             keyboardType="numeric"
+    //                             style={{ textAlignVertical: "top", padding: 10 }}
+    //                             onChangeText={(productPrice) => setProductPrice(productPrice)}
+    //                         />
+    //                         <Button title="Ürünü Güncelle" onPress={editProduct} />
+    //                     </KeyboardAvoidingView>
+    //                 </ScrollView>
+    //             </View>
+    //         </View>
+    //     </SafeAreaView>
+    // );
     return (
-
-        <SafeAreaView style={{ flex: 1 }}>
-            <View style={{ padding: 20 }}>
-                <TouchableOpacity onPress={() => navigation.goBack()} className="bg-yellow-400 p-2 rounded-tr-2xl rounded-bl-2xl ml-4">
-                    <ArrowLeftIcon size="20" color="black" />
-                </TouchableOpacity>
-            </View>
-            <View style={{ flex: 1, backgroundColor: 'white' }}>
-                <View style={{ flex: 1 }}>
-                    <ScrollView keyboardShouldPersistTaps="handled">
-                        <KeyboardAvoidingView
-                            behavior="padding"
-                            style={{ flex: 1, justifyContent: 'space-between' }}>
-                            <Text> Ürün Güncelleme Ekranı </Text>
-                            <TextInput placeholder="Ürün Adı"
-                                value={productName}
-                                editable={true}
-                                style={{ padding: 10 }}
-                                onChangeText={(productName) => setProductName(productName)} />
-                            <TextInput
-                                placeholder="Ürün Açıklaması"
-                                value={productDescription}
-                                editable={true}
-                                maxLength={255}
-                                numberOfLines={5}
-                                multiline={true}
-                                style={{ padding: 10 }}
-                                onChangeText={(productDescription) => setProductDescription(productDescription)} />
-                            <TextInput placeholder="Ürün Fiyatı"
-                                value={productPrice.toString()}
-                                editable={true}
-                                maxLength={5}
-                                keyboardType="numeric"
-                                style={{ textAlignVertical: 'top', padding: 10 }}
-                                onChangeText={(productPrice) => setProductPrice(productPrice)} />
-                            <Button title='Ürünü Güncelle' onPress={editProduct} />
-                        </KeyboardAvoidingView>
-                    </ScrollView>
-                </View>
-            </View>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <Header label="Ürün Güncelleme " />
+            <ScrollView keyboardShouldPersistTaps="handled">
+                <KeyboardAvoidingView behavior="padding" style={styles.content}>
+                    <Text style={styles.headerText}>Ürün Güncelleme</Text>
+                    <TextInput
+                        placeholder="Ürün Adı"
+                        value={productName}
+                        editable={true}
+                        style={styles.inputField}
+                        onChangeText={(productName) => setProductName(productName)}
+                    />
+                    <TextInput
+                        placeholder="Ürün Açıklaması"
+                        value={productDescription}
+                        editable={true}
+                        maxLength={255}
+                        numberOfLines={5}
+                        // multiline={true}
+                        style={styles.inputField}
+                        onChangeText={(productDescription) => setProductDescription(productDescription)}
+                    />
+                    <TextInput
+                        placeholder="Ürün Fiyatı"
+                        value={productPrice.toString()}
+                        editable={true}
+                        maxLength={5}
+                        keyboardType="numeric"
+                        style={styles.inputField}
+                        onChangeText={(productPrice) => setProductPrice(productPrice)}
+                    />
+                    <TouchableOpacity style={styles.editButton} onPress={editProduct}>
+                        <Text style={styles.editButtonText}>Ürünü Güncelle</Text>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
+            </ScrollView>
+        </View>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+
+    content: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 16,
+        marginTop: 16,
+        color: "#333",
+        marginLeft: 10,
+    },
+
+    inputField: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 16,
+        marginLeft: 10,
+        marginRight: 10,
+        borderWidth: 1,
+        borderColor: "#E5E3DD",
+        padding: 16,
+        borderRadius: 8,
+        marginLeft: 10,
+        fontSize: 18,
+        fontWeight: "semibold",
+        marginBottom: 8,
+        color: "#333",
+    },
+
+    editButton: {
+        backgroundColor: "#E5E3DD",
+        padding: 15,
+        marginLeft: 10,
+        width: "50%",
+        borderRadius: 20,
+    },
+    editButtonText: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#374151",
+        textAlign: "center",
+    },
+});
 export default EditProduct;
